3b40bc5fa30fbd8b69daca4915f07037526e9242
ColeKartchner <cole.kartchner99@gmail.com>
2023-01-30 11:02:23 -0700

    Changed line 42 of the galaga.gd
    Now the player doesn't die to their own bullets

--- game/Player.gd
+++ game/Player.gd
@@ -10,19 +10,19 @@ var sprite_size
 var num_lives = 3
 
 func _ready():
-    screen_size = get_viewport_rect().size
-    sprite_size = $Sprite.texture.get_size()
+	screen_size = get_viewport_rect().size
+	sprite_size = $Sprite.texture.get_size()
 
 func set_can_shoot(shoot_val):
-    can_shoot = shoot_val
+	can_shoot = shoot_val
 
 func _process(delta):
-    var velocity = 0
-    if Input.is_action_pressed("move_left"):
-        velocity -= 1
-    if Input.is_action_pressed("move_right"):
-        velocity += 1
-    if Input.is_action_just_pressed("fire"):
-        emit_signal("player_fire")
-    position.x += velocity * move_rate * delta
-    position.x = clamp(position.x, sprite_size.x / 2, screen_size.x - sprite_size.x / 2)
+	var velocity = 0
+	if Input.is_action_pressed("move_left"):
+		velocity -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity += 1
+	if Input.is_action_just_pressed("fire"):
+		emit_signal("player_fire")
+	position.x += velocity * move_rate * delta
+	position.x = clamp(position.x, sprite_size.x / 2, screen_size.x - sprite_size.x / 2)
