7d55417794e74f1b291c5044d1547d59476b1300
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2023-01-30 11:04:21 -0700

    deleted all imports

--- game/PlayerMissile.gd
+++ game/PlayerMissile.gd
@@ -6,19 +6,19 @@ var move_direction = Vector2(0, -1)
 var screen_size
 
 func get_class():
-    return "PlayerMissile"
+	return "PlayerMissile"
 
 func _ready():
-    screen_size = get_viewport_rect().size
+	screen_size = get_viewport_rect().size
 
 func _is_off_screen():
-    if position.x < 0 or position.x > screen_size.x \
-        or position.y < 0 or position.y > screen_size.y:
-            return true
-    return false
+	if position.x < 0 or position.x > screen_size.x \
+		or position.y < 0 or position.y > screen_size.y:
+			return true
+	return false
 
 func _process(delta):
-    position.x += move_rate * delta * move_direction.x
-    position.y += move_rate * delta * move_direction.y
-    if _is_off_screen():
-        queue_free()
+	position.x += move_rate * delta * move_direction.x
+	position.y += move_rate * delta * move_direction.y
+	if _is_off_screen():
+		queue_free()
