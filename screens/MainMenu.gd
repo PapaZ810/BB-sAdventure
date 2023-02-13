80f203f5f53222bd5ad2b1f9c3cf033b02608bf1
ColeKartchner <cole.kartchner99@gmail.com>
2023-02-13 10:10:31 -0700

    Updated Sprites
    Changed sprites

--- screens/MainMenu.gd
+++ screens/MainMenu.gd
@@ -7,19 +7,19 @@ signal about_pressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    navigation_buttons.append(
-        NavigationButton.new($VBoxContainer/NewGameButton, "new_game_pressed", "game")
-    )
-    navigation_buttons.append(
-        NavigationButton.new($VBoxContainer/HighScoresButton, "high_scores_pressed", "high-scores")
-    )
-    navigation_buttons.append(
-        NavigationButton.new($VBoxContainer/ControlsButton, "controls_pressed", "controls")
-    )
-    navigation_buttons.append(
-        NavigationButton.new($VBoxContainer/AboutButton, "about_pressed", "about")
-    )
-    connect_button_signals()
+	navigation_buttons.append(
+		NavigationButton.new($VBoxContainer/NewGameButton, "new_game_pressed", "game")
+	)
+	navigation_buttons.append(
+		NavigationButton.new($VBoxContainer/HighScoresButton, "high_scores_pressed", "high-scores")
+	)
+	navigation_buttons.append(
+		NavigationButton.new($VBoxContainer/ControlsButton, "controls_pressed", "controls")
+	)
+	navigation_buttons.append(
+		NavigationButton.new($VBoxContainer/AboutButton, "about_pressed", "about")
+	)
+	connect_button_signals()
 
 #func Controls():
 #    emit_signal("controls_pressed")
